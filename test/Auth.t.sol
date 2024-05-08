5e047203e6d5cf2c0b168247578aed5feab8d63e
Nicholas Rodrigues Lordello <nick@safe.global>
2024-05-08 14:32:10 +0200

    Connect AuthCall contrac with tests

--- test/Auth.t.sol
+++ test/Auth.t.sol
@@ -1,40 +1,15 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
-
-contract Invoker {
-    function commit() public view returns (bytes32 value) {
-        assembly ("memory-safe") {
-            value := extcodehash(address())
-        }
-    }
-
-    function burnForMe(uint256 amount, address authority, bytes calldata signature) external {
-        bytes memory authData = abi.encodePacked(signature, commit());
-
-        bool success;
-        assembly ("memory-safe") {
-            success := auth(authority, add(authData, 0x20), mload(authData))
-        }
-        require(success, "auth failed");
-
-        assembly ("memory-safe") {
-            if iszero(authcall(gas(), 0, amount, 0, 0, 0, 0)) {
-                returndatacopy(0, 0, returndatasize())
-                revert(0, returndatasize())
-            }
-        }
-    }
-}
+import {AuthCall} from "../src/AuthCall.sol";
 
 contract AuthTest is Test {
-    Invoker internal invoker = new Invoker();
+    AuthCall internal invoker = new AuthCall();
 
     function setUp() public {}
 
-    function test_Increment() public {
+    function test_MultiAuthCall() public {
         uint256 accountKey = uint256(keccak256("secret"));
         address account = vm.addr(accountKey);
 
@@ -44,18 +19,85 @@ contract AuthTest is Test {
                 block.chainid,
                 uint256(vm.getNonce(account)),
                 uint256(uint160(address(invoker))),
-                invoker.commit()
+                invoker.COMMIT()
             )
         );
 
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(accountKey, authDigest);
         bytes memory signature = abi.encodePacked(v - 27, r, s);
 
+        bytes memory transactions = abi.encodePacked(
+            abi.encodePacked(
+                uint8(0),
+                address(0),
+                uint256(1 gwei), // value
+                uint256(0), // data length
+                "" // data
+            ),
+            abi.encodePacked(
+                uint8(0),
+                address(0),
+                uint256(2 gwei), // value
+                uint256(0), // data length
+                "" // data
+            )
+        );
+
         vm.deal(account, 1 ether);
         assertEq(account.balance, 1 ether);
 
-        invoker.burnForMe(1 gwei, account, signature);
+        invoker.multiAuthCall(account, signature, transactions);
 
-        assertEq(account.balance, 1 ether - 1 gwei);
+        assertEq(account.balance, 1 ether - 3 gwei);
+    }
+
+    function test_MultiSendCompatibility() public {
+        uint256 accountKey = uint256(keccak256("secret"));
+        address account = vm.addr(accountKey);
+
+        bytes32 authDigest = keccak256(
+            abi.encodePacked(
+                uint8(0x04),
+                block.chainid,
+                uint256(vm.getNonce(account)),
+                uint256(uint160(address(invoker))),
+                invoker.COMMIT()
+            )
+        );
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(accountKey, authDigest);
+        bytes memory signature = abi.encodePacked(v - 27, r, s);
+
+        bytes memory transactions = abi.encodePacked(
+            // auth encoded in transactions
+            abi.encodePacked(
+                uint8(0),
+                account,
+                uint256(0), // ignored
+                signature.length,
+                signature
+            ),
+            abi.encodePacked(
+                uint8(0),
+                address(0),
+                uint256(1 gwei), // value
+                uint256(0), // data length
+                "" // data
+            ),
+            abi.encodePacked(
+                uint8(0),
+                address(0),
+                uint256(2 gwei), // value
+                uint256(0), // data length
+                "" // data
+            )
+        );
+
+        vm.deal(account, 1 ether);
+        assertEq(account.balance, 1 ether);
+
+        invoker.multiSend(transactions);
+
+        assertEq(account.balance, 1 ether - 3 gwei);
     }
 }
